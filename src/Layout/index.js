c4aae66c85b3ddd572b235fee84f5c75b270706f
Fadi Sayfi <fsayfi@icloud.com>
2023-12-18 07:18:23 -0500

    updated home page to show the deck list

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 
+import { listDecks } from '../utils/api/index';
+import { useState, useEffect } from 'react';
+import DeckList from "../Deck/DeckList";
+
 function Layout() {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    listDecks(abortController.signal).then(setDecks).catch((error) => console.log(error));
+    return () => abortController.abort();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
+        <div>
+          <button className="btn btn-secondary mb-2" onClick={() => window.location.href = "/decks/new"}>Create Deck</button>
+          <DeckList decks={decks} setDecks={setDecks} />
+        </div>
         <NotFound />
       </div>
     </>
